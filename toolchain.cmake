2ed781525f2bbab2d8942d06b01d2107161c12a1
Egor00f <velikiydolbayeb@gmail.com>
2024-11-09 16:15:56 +0000

    some fixes разделение флагов для Си и С++

--- toolchain.cmake
+++ toolchain.cmake
@@ -32,6 +32,8 @@ find_program (CMAKE_C_LINKER
 	REQUIRED
 )
 
+set(CMAKE_CXX_LINKER ${CMAKE_C_LINKER})
+
 find_program (CMAKE_AR
 	kos32-ar
 	PATHS ${TOOLCHAIN_PATH}/bin
@@ -64,23 +66,33 @@ set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
-
+#if KOLIBRIOS_REPO not defined, use default path
 if(NOT DEFINED KOLIBRIOS_REPO)
 	set(KOLIBRIOS_REPO ${CMAKE_CURRENT_SOURCE_DIR}/../../kolibrios)
 endif()
 
 get_filename_component(REPO ${KOLIBRIOS_REPO} REALPATH)
 
+#Check that kolibrios repo exists
+if(NOT EXISTS ${REPO}/contrib/sdk/sources)
+	message(FATAL "KolibriOS REPO not found")
+endif()
+
 set(SDK_DIR ${REPO}/contrib/sdk)
 set(NewLib_DIR ${SDK_DIR}/sources/newlib)
 set(Libstdcpp_DIR ${SDK_DIR}/sources/libstdc++-v3)
 
-add_compile_options(-fno-ident -fomit-frame-pointer -U__WIN32__ -U_Win32 -U_WIN32 -U__MINGW32__ -UWIN32 -I${NewLib_DIR}/libc/include -I${Libstdcpp_DIR}/include)
+#Set compiler flags
+set(CMAKE_C_FLAGS "-fno-ident -fomit-frame-pointer -U__WIN32__ -U_Win32 -U_WIN32 -U__MINGW32__ -UWIN32 -I${NewLib_DIR}/libc/include" CACHE STRING "KOLIBRIOS C COMPILER FLAGS")
+set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS} -I${Libstdcpp_DIR}/include" CACHE STRING "KOLIBRIOS CXX COMPILER FLAGS")
 
-add_link_options(-static -S --image-base 0 -nostdlib -T ${NewLib_DIR}/app.lds -L${TOOLCHAIN_PATH}/lib -L${TOOLCHAIN_PATH}/mingw32/lib -L${SDK_DIR}/lib --subsystem native)
+#Set linker flags
+set(CMAKE_C_LINKER_FLAGS "-nostdlib -static -S --image-base 0 -T ${NewLib_DIR}/app.lds -L${TOOLCHAIN_PATH}/lib -L${TOOLCHAIN_PATH}/mingw32/lib -L${SDK_DIR}/lib --subsystem native" CACHE STRING "KOLIBRIOS C LINKER FLAGS")
+set(CMAKE_CXX_LINKER_FLAGS ${CMAKE_C_LINKER_FLAGS} CACHE STRING "KOLIBRIOS CXX LINKER FLAGS")
 
-set(CMAKE_C_LINK_EXECUTABLE   "<CMAKE_LINKER> <LINK_FLAGS> -o <TARGET> <OBJECTS> --start-group -lgcc -lc.dll <LINK_LIBRARIES> --end-group <CMAKE_C_LINK_FLAGS>" CACHE STRING "KOLIBRIOS C LINK EXECUTABLE")
-set(CMAKE_CXX_LINK_EXECUTABLE "<CMAKE_LINKER> <LINK_FLAGS> -o <TARGET> <OBJECTS> --start-group -lgcc -lc.dll -lstdc++ -lsupc++ <LINK_LIBRARIES> --end-group <CMAKE_CXX_LINK_FLAGS>" CACHE STRING "KOLIBRIOS CXX LINK EXECUTABLE")
+
+set(CMAKE_C_LINK_EXECUTABLE   "<CMAKE_LINKER> <LINK_FLAGS> --start-group -lgcc -lc.dll <LINK_LIBRARIES> --end-group -o <TARGET> <OBJECTS>  <CMAKE_C_LINK_FLAGS>" CACHE STRING "KOLIBRIOS C LINK EXECUTABLE")
+set(CMAKE_CXX_LINK_EXECUTABLE "<CMAKE_LINKER> <LINK_FLAGS> --start-group -lgcc -lc.dll -lstdc++ -lsupc++ <LINK_LIBRARIES> --end-group -o <TARGET> <OBJECTS> <CMAKE_CXX_LINK_FLAGS>" CACHE STRING "KOLIBRIOS CXX LINK EXECUTABLE")
 
 
 set(CMAKE_C_COMPILER_FORCED TRUE)
