562c4e822bd1e8cf618382a2963ce733bff19321
Егор <148970641+Egor00f@users.noreply.github.com>
2024-11-13 06:39:54 +0000

    fix devcontainer

---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+sudo curl http://git.kolibrios.org/api/packages/Egor00f/debian/repository.key -o /etc/apt/keyrings/gitea-Egor00f.asc
+echo "deb [signed-by=/etc/apt/keyrings/gitea-Egor00f.asc] http://git.kolibrios.org/api/packages/Egor00f/debian bookworm main" | sudo tee -a /etc/apt/sources.list.d/gitea.list
+sudo apt update
+echo '/home/autobuild/tools' | sudo apt install kos32-gcc -y
+
